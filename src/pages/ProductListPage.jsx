e9010797d8c8bbcd94399e80262b0d4e91fcd966
pranish <pranishshrestha181@gmail.com>
2023-08-12 21:49:07 +0545

    feat: table added in product listing

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router";
+import { NavLink } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
 import { useInventory } from "../contexts/InventoryContext";
 import "./styles.css";
@@ -87,15 +88,38 @@ export const ProductListPage = () => {
           </label>
           <button onClick={() => navigate("/productMgt")}>New</button>
         </div>
+        <br />
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>stock</th>
+            <th>supplier</th>
+          </tr>
+          {sortedProducts.map(
+            ({ id, imageUrl, name, price, stock,description, supplier }) => (
+              <tr key={id}>
+                <td>
+                  <img src={imageUrl} alt={name} height={200} width={200}/>
+                </td>
+                <td><NavLink to={`/product/${id}`}>{name} </NavLink></td>
+                <td>{description}</td>
+                <td>{price}</td>
+                <td>{stock}</td>
+                <td>{supplier}</td>
+              </tr>
 
-        {sortedProducts.map(({ id, imageUrl, name, price, stock }) => (
-          <div key={id} onClick={() => navigate(`/product/${id}`)}>
-            <img src={imageUrl} alt={name} height={200} width={200} />
-            <h3>{name}</h3>
-            <p>Price: ${price.toFixed(2)}</p>
-            <p>Stock: {stock}</p>
-          </div>
-        ))}
+              //   <div key={id} onClick={() => navigate(`/product/${id}`)}>
+              //     <img src={imageUrl} alt={name} height={200} width={200} />
+              //     <h3>{name}</h3>
+              //     <p>Price: ${price.toFixed(2)}</p>
+              //     <p>Stock: {stock}</p>
+              //   </div>
+            )
+          )}
+        </table>
       </div>
     </div>
   );
